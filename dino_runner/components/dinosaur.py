09eafa227e26e431e20f5c3df0c86ef5a259cac1
MISS-DAYANNA-C <110805682+MISS-DAYANNA@users.noreply.github.com>
2022-08-09 20:46:47 -0400

    CLASS 0-Add Dino folder

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,11 @@
+class Dinosaur:
+    def __init(self):
+        pass
+    def update(self):
+        pass
+    def duck(self):
+        pass
+    def run(self):
+        pass
+    def draw(self):
+        pass
